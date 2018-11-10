daa1c20b0cdcd795495d836d02e0d23ebfd49e13
ControllerPrime <31867665+ControllerPrime@users.noreply.github.com>
2018-11-10 17:47:05 -0500

    Finished but not work
    I finished the final step but i missed something and it doesn't work

--- Assets/__Scripts/ScoreManager.cs
+++ Assets/__Scripts/ScoreManager.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+// an enum to handle all the possibe scoring events
 public enum eScoreEvent
 {
     draw,
@@ -11,38 +12,41 @@ public enum eScoreEvent
     gameLoss
 }
 
-public class ScoreManager : MonoBehaviour {
+public class ScoreManager : MonoBehaviour
+{
     static private ScoreManager S;
 
     static public int SCORE_FROM_PREV_ROUND = 0;
     static public int HIGH_SCORE = 0;
 
-    [Header("Set Dynamically")]
+    [Header("set Dynamically")]
     public int chain = 0;
     public int scoreRun = 0;
     public int score = 0;
 
-	// Use this for initialization
-	void Awake () {
-		if (S == null)
+    private void Awake()
+    {
+        if (S == null)
         {
             S = this;
         }
         else
         {
-            Debug.LogError("ERROR: ScoreManager.Awake(): S is already set!");
+            Debug.LogError("ERROR: ScoreManager.Awake(); S is already set!");
         }
 
+        // check for highscore
         if (PlayerPrefs.HasKey("ProspectorHighScore"))
         {
             HIGH_SCORE = PlayerPrefs.GetInt("ProspectorHighScore");
         }
-
+        // add the score from the last round, which will be > 0 if it was a win
         score += SCORE_FROM_PREV_ROUND;
+        // and reset
         SCORE_FROM_PREV_ROUND = 0;
-	}
+    }
 
-    static public void EVENT (eScoreEvent evt)
+    static public void EVENT(eScoreEvent evt)
     {
         try
         {
@@ -53,48 +57,54 @@ public class ScoreManager : MonoBehaviour {
             Debug.LogError("ScoreManager:EVENT() called while S = null.\n" + nre);
         }
     }
-	
-	void Event (eScoreEvent evt) {
-		switch (evt)
+
+    void Event(eScoreEvent evt)
+    {
+        switch (evt)
         {
-            case eScoreEvent.draw:
-            case eScoreEvent.gameWin:
-            case eScoreEvent.gameLoss:
-                chain = 0;
-                score += scoreRun;
-                scoreRun = 0;
+            // same things need to happen regardless of the game outcome
+            case eScoreEvent.draw:          // drawing a card 
+            case eScoreEvent.gameWin:       // won the round
+            case eScoreEvent.gameLoss:      // lost the round
+                chain = 0;                  // resets the score chain
+                score += scoreRun;          // add scoreRun to total score
+                scoreRun = 0;               // reset scoreRun
                 break;
 
-            case eScoreEvent.mine:
-                chain++;
-                scoreRun += chain;
+            case eScoreEvent.mine:          // remove a mine card
+                chain++;                    // increase the score chain
+                scoreRun += chain;          // add score for this card to run
                 break;
         }
+
+        // handles win/loss
         switch (evt)
         {
             case eScoreEvent.gameWin:
+                // if its a win, add the score to the next round
                 SCORE_FROM_PREV_ROUND = score;
-                print ("You won this round! Round score: " + score);
+                print("You won this round! Keep it up. Round score: " + score);
                 break;
 
             case eScoreEvent.gameLoss:
+                // if its a loss, check against the high score
                 if (HIGH_SCORE <= score)
                 {
-                    print ("You got the high score! High score: " + score);
+                    print("You got the high score! High score: " + score);
                     HIGH_SCORE = score;
                     PlayerPrefs.SetInt("ProspectorHighScore", score);
                 }
                 else
                 {
-                    print ("Your final score for the game was: " + score);
+                    print("Your final score for the game was: " + score);
                 }
                 break;
 
             default:
-                print("score: " + score + " scoreRun:" + scoreRun + " chain: " + chain);
+                print("score: " + score + " scoreRun: " + scoreRun + " chain: " + chain);
                 break;
         }
-	}
+    }
 
     static public int CHAIN { get { return S.chain; } }
     static public int SCORE { get { return S.score; } }
